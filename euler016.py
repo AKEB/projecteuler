4adb6f05da398cab2e68b4b6feb0d0fca9298ce5
Vadim Babajanyan <akeb@akeb.ru>
2022-04-27 04:17:03 +0300

    fix description

--- euler016.py
+++ euler016.py
@@ -1,10 +1,10 @@
 """
-215 = 32768, сумма цифр этого числа равна 3 + 2 + 7 + 6 + 8 = 26.
-Какова сумма цифр числа 21000?
+2**15 = 32768, сумма цифр этого числа равна 3 + 2 + 7 + 6 + 8 = 26.
+Какова сумма цифр числа 2**1000?
 https://euler.jakumo.org/problems/view/16.html
 
-215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-What is the sum of the digits of the number 21000?
+2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+What is the sum of the digits of the number 2**1000?
 https://projecteuler.net/problem=16
 """
 
